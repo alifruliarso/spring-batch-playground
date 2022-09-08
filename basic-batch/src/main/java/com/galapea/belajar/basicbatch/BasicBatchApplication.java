645bb60ef35cd8a54d7dc991ca2b5448c8c6b2ff
ruli <alif.ruliarso@gmail.com>
2022-09-08 18:48:00 +0700

    generate project

---
+++ basic-batch/src/main/java/com/galapea/belajar/basicbatch/BasicBatchApplication.java
@@ -0,0 +1,13 @@
+package com.galapea.belajar.basicbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicBatchApplication.class, args);
+	}
+
+}
---
+++ basic-batch/src/test/java/com/galapea/belajar/basicbatch/BasicBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galapea.belajar.basicbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
